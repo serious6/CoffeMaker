f47e6bafd8693361ae475cf9ecab9569bc716481
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-04-08 10:33:24 +0200

    Fix JunitSuite

--- unittests/edu/ncsu/csc326/coffeemaker/suite/AllTests.java
+++ unittests/edu/ncsu/csc326/coffeemaker/suite/AllTests.java
@@ -4,11 +4,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import edu.ncsu.csc326.coffeemaker.CoffeeMakerTest;
 import edu.ncsu.csc326.coffeemaker.AddRecipeTest;
+import edu.ncsu.csc326.coffeemaker.CheckInventoryTest;
+import edu.ncsu.csc326.coffeemaker.CoffeeMakerTest;
+import edu.ncsu.csc326.coffeemaker.DeleteRecipeTest;
+import edu.ncsu.csc326.coffeemaker.EditRecipeTest;
+import edu.ncsu.csc326.coffeemaker.PurchaseCoffeeTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({CoffeeMakerTest.class, AddRecipeTest.class})
+@SuiteClasses({ CoffeeMakerTest.class, AddRecipeTest.class,
+		CheckInventoryTest.class, DeleteRecipeTest.class, EditRecipeTest.class,
+		PurchaseCoffeeTest.class })
 public class AllTests {
 
 }
